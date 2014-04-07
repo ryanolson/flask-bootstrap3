c77141461eceb494acd543c4025ace0e3954d42a
Ryan Olson <ryan@cray.com>
2014-04-07 13:19:18 -0600

    bumped version to 3.1.1.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-Bootstrap3',
-    version='3.1.1.2',
+    version='3.1.1.3',
     author="Ryan Olson",
     author_email="rmolson@gmail.com",
     url="https://github.com/ryanolson/flask-bootstrap3",
